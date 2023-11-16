3ca4d19c6c2910a7e74498dfc7b432a5a0f01b8d
Ashish Sunny <ashishsunny.dev@gmail.com>
2023-11-16 22:54:54 +0530

    no major updates

--- app/invoices-page/page.tsx
+++ app/invoices-page/page.tsx
@@ -4,10 +4,20 @@ import InvoiceComponent from "@/components/invoice_component/invoice_component"
 
 
 const InvoicesPage : React.FC= () => {
+  
+  const invoicecardProps = {
+    // classN: CSSProperties,
+    rNo: 'RT3080',
+    date: 'Due 19 Aug 2021',
+    price: '1,800.90',
+    name: 'Jensen Huang',
+    status: 'Paid',
+  };
+
   return (
     <div className="h-screen w-screen bg-color1">
         <FilterBar />
-        <InvoiceComponent/>
+        <InvoiceComponent {...invoicecardProps} />
     </div>
   )
 }
--- components/invoice_component/invoice_component.tsx
+++ components/invoice_component/invoice_component.tsx
@@ -3,23 +3,28 @@ import React, { CSSProperties } from 'react'
 
 
 interface MyComponentProps {
-    classN?: CSSProperties; 
+    classN?: CSSProperties;
+    rNo?: string;
+    date?: string;
+    price?: string;
+    name?: string;
+    status?: string;
 }
 
-const InvoiceComponent: React.FC<MyComponentProps> = ({ classN }) => {
+const InvoiceComponent: React.FC<MyComponentProps> = ({ classN, rNo, date, price, name }) => {
   return (
     <div className='rounded-[0.5rem] mx-[1.5rem] h-[8.3rem] bg-color3 text-white'>
 
         <div className='p-[1.5rem] flex justify-between items-center'>
 
         <div>
-            <div className='flex'><p className='text-steelblue font-bold'>#</p><p className='font-bold'>RT3080</p></div> 
-            <p className='pt-[1.5rem] text-[.8rem] text-color2'>Due 19 Aug 2021</p>
-            <p className='pt-[0.5rem] font-bold'>$ 1,800.90</p>
+            <div className='flex'><p className='text-steelblue font-bold'>#</p><p className='font-bold'>{rNo}</p></div> 
+            <p className='pt-[1.5rem] text-[.8rem] text-color2'>{date}</p>
+            <div className='flex items-center pt-[0.7rem]'><p className='font-bold mr-[.3rem]'>$</p><p className='font-bold'>{price}</p></div>
         </div>
 
         <div>
-            <p>Jensen Huang</p>
+            <p>{name}</p>
             <div className=' bg-success mt-[1.5rem] bg-opacity-[0.05] flex rounded-[.375rem]'>
                 <div className='flex w-[6.2rem] h-[2.5rem] px-[1.8rem] py-[.8rem]'>
                 <div className='flex items-center pr-[.5rem]'>
